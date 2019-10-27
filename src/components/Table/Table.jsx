149c75719228b7fb13e028203a3f94eb42a4ed59
simonas <simonas.zaronskis@devbridge.com>
2019-10-27 13:50:06 +0200

    Load the hard coded example campaigns data to the table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -3,27 +3,23 @@ import ReactTable from 'react-table';
 import PropTypes from 'prop-types';
 import { IntlProvider } from 'react-intl';
 import 'react-table/react-table.css';
+import campaigns from './data';
 
-const data = [
-  {
-    name: 'Tanner Linsley',
-    age: 26,
-  },
-  {
-    name: 'Josh Peterson',
-    age: 30,
-  },
+const columns = [
+  'column.name',
+  'column.startDate',
+  'column.endDate',
+  'column.Budget',
+  'column.userId',
 ];
 
-const columnsToTranslate = ['column.name', 'column.age'];
-
 const Table = ({ intl }) => {
-  const translatedColumns = columnsToTranslate.map((id) => ({
+  const translatedColumns = columns.map((id) => ({
     Header: intl.formatMessage({ id }),
     accessor: id.split('.').pop(),
   }));
 
-  return <ReactTable data={data} columns={translatedColumns} />;
+  return <ReactTable data={campaigns} columns={translatedColumns} />;
 };
 
 Table.propTypes = {
