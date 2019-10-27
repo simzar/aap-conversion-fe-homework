73b5a5b700139ebf4c3a30942dc39e61282c8334
simonas <simonas.zaronskis@devbridge.com>
2019-10-27 13:58:08 +0200

    Updates

---
+++ precache-manifest.852c8503fc55c7eda81c4d088c431f61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d374ba336b1529bbc90ae872e740862",
+    "url": "/simzar/aap-conversion-fe-homework/index.html"
+  },
+  {
+    "revision": "894bdd65208e1a3a5014",
+    "url": "/simzar/aap-conversion-fe-homework/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "2d1275bd8aa5a3be15a6",
+    "url": "/simzar/aap-conversion-fe-homework/static/css/main.7e6afcbd.chunk.css"
+  },
+  {
+    "revision": "894bdd65208e1a3a5014",
+    "url": "/simzar/aap-conversion-fe-homework/static/js/2.2d507ea9.chunk.js"
+  },
+  {
+    "revision": "2d1275bd8aa5a3be15a6",
+    "url": "/simzar/aap-conversion-fe-homework/static/js/main.fce2316e.chunk.js"
+  },
+  {
+    "revision": "0166b5ea2e295f9d9225",
+    "url": "/simzar/aap-conversion-fe-homework/static/js/runtime-main.4d5a5e76.js"
+  }
+]);
